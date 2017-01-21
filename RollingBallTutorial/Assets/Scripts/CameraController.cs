73389d11b87f2d9d6a06397573bbc8422d516a2b
adrianafitz <adrianafitz@gmail.com>
2017-01-20 23:40:51 -0600

    Rolling ball tutorial
    rolling ball tutorial

---
+++ RollingBallTutorial/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    //each frame before displaying what the camera can see, the camera is moved
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called once per frame
+    //Guaranteed to run after all lines are executed in Update()
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ RollingBallTutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Check every frame for player input
+    //Apply input every frame as movement
+
+    private Rigidbody rb;
+    public float speed;  //should show up in the inspector
+
+    //Called on the first frame the script is active, often first frame of game
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()  //called before applying physics, movement code
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);  //determine force added to ball
+
+        rb.AddForce(movement * speed);
+    }    
+}
