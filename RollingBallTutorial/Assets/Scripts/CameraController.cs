fcb99657d5bf3dbb0757bb3f52e589e88fcc04ec
adrianafitz <adrianafitz@gmail.com>
2017-01-21 00:31:00 -0600

    Jumping, bounciness, removed camera attachment
    Jumping, bounciness, removed camera attachment

--- RollingBallTutorial/Assets/Scripts/CameraController.cs
+++ RollingBallTutorial/Assets/Scripts/CameraController.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+    //public GameObject player;
 
-    private Vector3 offset;
+    //private Vector3 offset;
 
-    //each frame before displaying what the camera can see, the camera is moved
+    ////each frame before displaying what the camera can see, the camera is moved
 
-    // Use this for initialization
-    void Start()
-    {
-        offset = transform.position - player.transform.position;
-    }
+    //// Use this for initialization
+    //void Start()
+    //{
+    //    offset = transform.position - player.transform.position;
+    //}
 
-    // LateUpdate is called once per frame
-    //Guaranteed to run after all lines are executed in Update()
-    void LateUpdate()
-    {
-        transform.position = player.transform.position + offset;
-    }
+    //// LateUpdate is called once per frame
+    ////Guaranteed to run after all lines are executed in Update()
+    //void LateUpdate()
+    //{
+    //    transform.position = player.transform.position + offset;
+    //}
 }
--- RollingBallTutorial/Assets/Scripts/Player1Controller.cs
+++ RollingBallTutorial/Assets/Scripts/Player1Controller.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Player1Controller : MonoBehaviour
 {
 
     //Check every frame for player input
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
     public float speed;  //should show up in the inspector
+    public float jumpForce; //should show up in the inspector
 
     //Called on the first frame the script is active, often first frame of game
     void Start()
@@ -17,10 +18,26 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            print("space key was pressed");
+            Jump();          
+        }
+    }
+
+    public void Jump()
+    {
+        Vector3 up = transform.TransformDirection(Vector3.up);
+        rb.AddForce(up * jumpForce, ForceMode.Impulse);
+    }
+
     private void FixedUpdate()  //called before applying physics, movement code
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);  //determine force added to ball
 
         rb.AddForce(movement * speed);
