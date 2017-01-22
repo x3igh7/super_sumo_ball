c25dae89671b7df13f413040c5f7ec5dc675e6df
adrianafitz <adrianafitz@gmail.com>
2017-01-22 12:50:43 -0600

    endscene
    endscene

---
+++ SuperSumoBall/Assets/Scripts/DisplayWinner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DisplayWinner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int winningPlayer;
+        winningPlayer= PlayerPrefs.GetInt("Winner");
+
+        //if (winningPlayer == 0)
+        //{
+        //    var winnerInstance = Instantiate(PLAYER1WIN, Vector3(5, 1, 0), Quaternion.identity);
+        //}
+        //else if (winningPlayer == 1)
+        //{
+        //    var winnerInstance = Instantiate(PLAYER2WIN, Vector3(5, 1, 0), Quaternion.identity);
+        //}
+        //else if (winningPlayer == 2)
+        //{
+        //    var winnerInstance = Instantiate(PLAYER3WIN, Vector3(5, 1, 0), Quaternion.identity);
+        //}
+        //else if (winningPlayer == 3)
+        //{
+        //    var winnerInstance = Instantiate(PLAYER4WIN, Vector3(5, 1, 0), Quaternion.identity);
+        //}
+    }
+}
