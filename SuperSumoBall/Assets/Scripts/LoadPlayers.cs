faf36abb707ded33562e32b756396585f06d1dfa
Conner Smith <connerpsmith@gmail.com>
2017-01-21 22:07:07 -0600

    changing to 2 players

---
+++ SuperSumoBall/Assets/Scripts/LoadPlayers.cs
@@ -0,0 +1,45 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class LoadPlayers : MonoBehaviour {
+    public GameObject player;
+    public int numberOfPlayers;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 1; i <= numberOfPlayers; i++)
+        {
+            var playerInstance = Instantiate(player, GetPlayerVector(i), Quaternion.identity);
+            playerInstance.gameObject.name = "Player" + i;
+            MonoBehaviour[] list = playerInstance.gameObject.GetComponents<MonoBehaviour>();
+
+            foreach (var mb in list)
+            {
+                if (mb is PlayerController)
+                {
+                    (mb as PlayerController).PlayerNum = i;
+                    (mb as PlayerController).jumpButton = "Jump" + i;
+                    (mb as PlayerController).horizontalButton = "Horizontal" + i;
+                    (mb as PlayerController).verticalButton = "Vertical" + i;
+                }
+            }
+        }
+	}
+
+    private Vector3 GetPlayerVector(int playerNumber)
+    {
+        switch (playerNumber)
+        {
+            case 1:
+                return new Vector3(5, 1, 0);
+            case 2:
+                return new Vector3(-5, 1, 0);
+            case 3:
+                return new Vector3(0, 1, 5);
+            case 4:
+                return new Vector3(0, 1, -5);
+            default:
+                return new Vector3();
+        }
+    }
+}
